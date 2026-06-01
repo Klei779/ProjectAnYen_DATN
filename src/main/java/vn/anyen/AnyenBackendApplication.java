5f3b53298d010c8391ba7189519dda17be14cc03
Le Trung Kien <kienltts01654@gmail.com>
2026-06-01 09:31:45 +0700

    Initial Spring Boot project setup

---
+++ src/main/java/vn/anyen/AnyenBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.anyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyenBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/anyen/AnyenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.anyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

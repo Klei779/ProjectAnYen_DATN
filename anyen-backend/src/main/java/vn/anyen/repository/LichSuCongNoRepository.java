45047b8158ecc54102a5126dad1bf6698811fee0
Le Trung Kien <kienltts01654@gmail.com>
2026-07-09 09:26:20 +0700

    Merge stash into kien

---
+++ anyen-backend/src/main/java/vn/anyen/repository/LichSuCongNoRepository.java
@@ -0,0 +1,17 @@
+package vn.anyen.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import vn.anyen.entity.LichSuCongNo;
+
+import java.util.List;
+
+@Repository
+public interface LichSuCongNoRepository extends JpaRepository<LichSuCongNo, Integer> {
+    
+    List<LichSuCongNo> findByCongNo_MaCongNo(Integer maCongNo);
+    
+    Page<LichSuCongNo> findAll(Pageable pageable);
+}

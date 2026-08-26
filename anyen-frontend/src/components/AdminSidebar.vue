e3d8cbb0ee4d0f1bfe0510cf106d5377b81aee3b
Mingtree2008 <tn098738@gmail.com>
2026-08-26 13:04:49 +0700

    hotfix sidebar

--- anyen-frontend/src/components/AdminSidebar.vue
+++ anyen-frontend/src/components/AdminSidebar.vue
@@ -30,34 +30,6 @@
     </div>
 
     <nav class="menu">
-<<<<<<< Updated upstream
-      <!-- Quản lý nhân sự -->
-      <div class="menu-group">
-        <div class="menu-group-header" @click="toggleMenu('users')">
-          <i class="fa-solid fa-users-gear menu-icon"></i>
-          <span class="sidebar-text">Quản lý nhân sự</span>
-          <i class="fa-solid menu-chevron" :class="expandedMenus.users ? 'fa-chevron-down' : 'fa-chevron-right'"></i>
-        </div>
-        <transition name="slide-fade">
-          <div class="submenu" v-if="expandedMenus.users">
-            <RouterLink to="/admin/quan-ly-doi-tac" class="menu-item submenu-item" title="Quản lý đối tác">
-              <i class="fa-solid fa-handshake menu-icon"></i>
-              <span class="sidebar-text">Quản lý đối tác</span>
-            </RouterLink>
-            <RouterLink to="/admin/quan-ly-nhan-vien" class="menu-item submenu-item" title="Quản lý nhân viên">
-              <i class="fa-solid fa-user-tie menu-icon"></i>
-              <span class="sidebar-text">Quản lý nhân viên</span>
-            </RouterLink>
-          </div>
-        </transition>
-      </div>
-
-      <!-- Quản lý khách hàng -->
-      <RouterLink to="/admin/quan-ly-khach-hang" class="menu-item" title="Quản lý khách hàng">
-        <i class="fa-solid fa-user menu-icon"></i>
-        <span class="sidebar-text">Quản lý khách hàng</span>
-      </RouterLink>
-=======
 
       <RouterLink to="/admin/quan-ly-doi-tac" class="menu-item" title="Quản lý đối tác">
         <i class="fa-solid fa-handshake menu-icon"></i>
@@ -101,81 +73,15 @@
              fa-newspaper
              menu-icon"
         ></i>
->>>>>>> Stashed changes
 
-      <!-- Đơn hàng & Hợp đồng -->
-      <div class="menu-group">
-        <div class="menu-group-header" @click="toggleMenu('orders')">
-          <i class="fa-solid fa-clipboard-list menu-icon"></i>
-          <span class="sidebar-text">Đơn hàng & Hợp đồng</span>
-          <i class="fa-solid menu-chevron" :class="expandedMenus.orders ? 'fa-chevron-down' : 'fa-chevron-right'"></i>
-        </div>
-        <transition name="slide-fade">
-          <div class="submenu" v-if="expandedMenus.orders">
-            <RouterLink to="/admin/quan-ly-don-hang" class="menu-item submenu-item" title="Quản lý đơn hàng">
-              <i class="fa-solid fa-box menu-icon"></i>
-              <span class="sidebar-text">Quản lý đơn hàng</span>
-            </RouterLink>
-            <RouterLink to="/admin/quan-ly-hop-dong" class="menu-item submenu-item" title="Quản lý hợp đồng">
-              <i class="fa-solid fa-file-contract menu-icon"></i>
-              <span class="sidebar-text">Quản lý hợp đồng</span>
-            </RouterLink>
-            <RouterLink to="/admin/quan-ly-hoa-don" class="menu-item submenu-item" title="Quản lý hóa đơn">
-              <i class="fa-solid fa-file-invoice-dollar menu-icon"></i>
-              <span class="sidebar-text">Quản lý hóa đơn</span>
-            </RouterLink>
-          </div>
-        </transition>
-      </div>
-
-      <!-- Quản lý nội dung -->
-      <div class="menu-group">
-        <div class="menu-group-header" @click="toggleMenu('content')">
-          <i class="fa-solid fa-newspaper menu-icon"></i>
-          <span class="sidebar-text">Quản lý nội dung</span>
-          <i class="fa-solid menu-chevron" :class="expandedMenus.content ? 'fa-chevron-down' : 'fa-chevron-right'"></i>
-        </div>
-        <transition name="slide-fade">
-          <div class="submenu" v-if="expandedMenus.content">
-            <RouterLink to="/admin/duyet-san-pham" class="menu-item submenu-item" title="Quản lý sản phẩm">
-              <i class="fa-solid fa-box-open menu-icon"></i>
-              <span class="sidebar-text">Quản lý sản phẩm</span>
-            </RouterLink>
-            <RouterLink to="/admin/quan-ly-tin-tuc" class="menu-item submenu-item" title="Quản lý tin tức">
-              <i class="fa-regular fa-newspaper menu-icon"></i>
-              <span class="sidebar-text">Quản lý tin tức</span>
-            </RouterLink>
-          </div>
-        </transition>
-      </div>
-
-      <!-- Quản lý tài chính -->
-      <div class="menu-group">
-        <div class="menu-group-header" @click="toggleMenu('finance')">
-          <i class="fa-solid fa-coins menu-icon"></i>
-          <span class="sidebar-text">Quản lý tài chính</span>
-          <i class="fa-solid menu-chevron" :class="expandedMenus.finance ? 'fa-chevron-down' : 'fa-chevron-right'"></i>
-        </div>
-        <transition name="slide-fade">
-          <div class="submenu" v-if="expandedMenus.finance">
-            <RouterLink to="/admin/quan-ly-cong-no" class="menu-item submenu-item" title="Quản lý công nợ">
-              <i class="fa-solid fa-file-invoice menu-icon"></i>
-              <span class="sidebar-text">Quản lý công nợ</span>
-            </RouterLink>
-            <RouterLink to="/admin/thong-ke-doanh-thu" class="menu-item submenu-item" title="Thống kê doanh thu">
-              <i class="fa-solid fa-chart-line menu-icon"></i>
-              <span class="sidebar-text">Thống kê doanh thu</span>
-            </RouterLink>
-          </div>
-        </transition>
-      </div>
-
+        <span class="sidebar-text">
+    Quản lý tin tức
+  </span>
+      </RouterLink>
       <RouterLink to="/admin/thong-bao" class="menu-item" title="Thông báo">
         <i class="fa-solid fa-bell menu-icon"></i>
         <span class="sidebar-text">Thông báo</span>
       </RouterLink>
-<<<<<<< Updated upstream
-=======
       <RouterLink to="/admin/thong-ke-doanh-thu" class="menu-item" title="Thống kê doanh thu">
         <i class="fa-solid fa-chart-line menu-icon"></i>
         <span class="sidebar-text">Thống kê doanh thu</span>
@@ -196,7 +102,6 @@
         <i class="fa-solid fa-file-invoice-dollar menu-icon"></i>
         <span class="sidebar-text">Quản lý công nợ</span>
       </RouterLink>
->>>>>>> Stashed changes
       <RouterLink to="/admin/thong-tin-tai-khoan" class="menu-item" title="Thông tin tài khoản">
         <i class="fa-solid fa-user menu-icon"></i>
         <span class="sidebar-text">Thông tin tài khoản</span>
@@ -225,13 +130,6 @@ const isCollapsed = ref(false);
 const showProfile = ref(false);
 const user = ref(null);
 
-const expandedMenus = ref({
-  users: false,
-  orders: false,
-  content: false,
-  finance: false
-});
-
 onMounted(() => {
   const userStr = localStorage.getItem("user");
   if (userStr) {
@@ -256,10 +154,6 @@ const toggleSidebar = () => {
 
 const toggleProfile = () => {
   showProfile.value = !showProfile.value;
-};
-
-const toggleMenu = (menuName) => {
-  expandedMenus.value[menuName] = !expandedMenus.value[menuName];
 };
 
 const logout = () => {
--- anyen-frontend/src/components/DoiTacSidebar.vue
+++ anyen-frontend/src/components/DoiTacSidebar.vue
@@ -35,24 +35,9 @@
         <span class="sidebar-text">Quản lý sản phẩm</span>
       </RouterLink>
 
-<<<<<<< Updated upstream
-      <RouterLink to="/doi-tac/quan-ly-combo" class="menu-item" title="Quản lý combo">
-        <i class="fa-solid fa-layer-group menu-icon"></i>
-        <span class="sidebar-text">Quản lý combo</span>
-      </RouterLink>
-
-      <RouterLink
-          to="/doi-tac/tao-combo"
-          class="menu-item"
-          title="Tạo Combo"
-      >
-        <i class="fa-solid fa-boxes-stacked menu-icon"></i>
-        <span class="sidebar-text">Tạo Combo</span>
-=======
       <RouterLink to="/doi-tac/tao-san-pham" class="menu-item" title="Tạo sản phẩm">
         <i class="fa-solid fa-circle-plus menu-icon"></i>
         <span class="sidebar-text">Tạo sản phẩm</span>
->>>>>>> Stashed changes
       </RouterLink>
 
       <RouterLink to="/doi-tac/quan-ly-don-hang" class="menu-item" title="Quản lý đơn hàng">
@@ -64,28 +49,27 @@
         <i class="fa-solid fa-bell menu-icon"></i>
         <span class="sidebar-text">Thông báo</span>
       </RouterLink>
+      <RouterLink
+          to="/doi-tac/quy-va-vi"
+          class="menu-item"
+          title="Quỹ & Ví"
+      >
+        <i
+            class="
+        fa-solid
+        fa-wallet
+        menu-icon
+      "
+        ></i>
 
-      <!-- Quản lý tài chính -->
-      <div class="menu-group">
-        <div class="menu-group-header" @click="toggleMenu('finance')">
-          <i class="fa-solid fa-coins menu-icon"></i>
-          <span class="sidebar-text">Quản lý tài chính</span>
-          <i class="fa-solid menu-chevron" :class="expandedMenus.finance ? 'fa-chevron-down' : 'fa-chevron-right'"></i>
-        </div>
-        <transition name="slide-fade">
-          <div class="submenu" v-if="expandedMenus.finance">
-            <RouterLink to="/doi-tac/quy-va-vi" class="menu-item submenu-item" title="Quỹ & Ví">
-              <i class="fa-solid fa-wallet menu-icon"></i>
-              <span class="sidebar-text">Quỹ & Ví</span>
-            </RouterLink>
-            <RouterLink to="/doi-tac/thong-ke-doanh-thu" class="menu-item submenu-item" title="Thống kê doanh thu">
-              <i class="fa-solid fa-chart-line menu-icon"></i>
-              <span class="sidebar-text">Thống kê doanh thu</span>
-            </RouterLink>
-          </div>
-        </transition>
-      </div>
-
+        <span class="sidebar-text">
+    Quỹ & Ví
+  </span>
+      </RouterLink>
+      <RouterLink to="/doi-tac/thong-ke-doanh-thu" class="menu-item" title="Thống kê doanh thu">
+        <i class="fa-solid fa-chart-line menu-icon"></i>
+        <span class="sidebar-text">Thống kê doanh thu</span>
+      </RouterLink>
       <RouterLink to="/doi-tac/thong-tin-tai-khoan" class="menu-item" title="Thông tin tài khoản">
         <i class="fa-solid fa-user menu-icon"></i>
         <span class="sidebar-text">Thông tin tài khoản</span>
@@ -115,10 +99,6 @@ const isCollapsed = ref(false);
 const showProfile = ref(false);
 const user = ref(null);
 
-const expandedMenus = ref({
-  finance: false
-});
-
 onMounted(() => {
   const userStr = localStorage.getItem("user");
   if (userStr) {
@@ -139,10 +119,6 @@ const closeProfile = (e) => {
 
 const toggleSidebar = () => {
   isCollapsed.value = !isCollapsed.value;
-};
-
-const toggleMenu = (menuName) => {
-  expandedMenus.value[menuName] = !expandedMenus.value[menuName];
 };
 
 const toggleProfile = () => {
